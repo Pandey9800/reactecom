42f1fbc7b601d94e47fd1eabbb72d311f480496b
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2024-07-04 23:24:37 +0530

    \

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {styles} from './header.module.css'
+import styles from './header.module.css'
 
 const Header = () => {
   // return <div className='header'>Header</div>
-  return <div className={styles.Header}>Header</div>
+  return <div className={styles.header}>Header</div>
 }
 
 export default Header
